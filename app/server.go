f883c2f8b59eb023cbf740a52d5b0545445e412a
aelouadi <elouadi.abdelali1997@gmail.com>
2024-06-09 01:36:51 +0100

    add ping commad

--- app/server.go
+++ app/server.go
@@ -1,26 +1,49 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	// Uncomment this block to pass the first stage
-	 "net"
-	 "os"
+	"net"
+	"os"
+	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	fmt.Println("server listenning on port 6379")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-	 	fmt.Println("Failed to bind to port 6379")
-	 	os.Exit(1)
-	 }
-	 _, err = l.Accept()
-	 if err != nil {
-	 	fmt.Println("Error accepting connection: ", err.Error())
-	 	os.Exit(1)
-	 }
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	handleConnection(conn)
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("the the addrssing of the connection is ", conn.RemoteAddr())
+	reader := bufio.NewReader(conn)
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("error while reading data")
+		}
+		cleanMessage := strings.TrimSpace(message)
+		fmt.Println(cleanMessage)
+		if strings.Compare(cleanMessage, "ping") == 0 {
+			_, err = conn.Write([]byte("+PONG\r\n"))
+			if err != nil {
+				os.Exit(1)
+			}
+		}
+		os.Exit(0)
+	}
+
 }
